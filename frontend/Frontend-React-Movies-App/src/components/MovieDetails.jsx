3ff6a0293050aeac9e2d3e2b6f8567320cbc9792
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2025-10-06 11:24:16 +0530

    Movie Details Displaying Component

---
+++ frontend/Frontend-React-Movies-App/src/components/MovieDetails.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { useMovieDetails } from '../API/Tmdb';
+import CastingDetails from './CastingDetails';
+import { ClassNames } from '@emotion/react';
+import RoundProgress from '../ui/RoundProgress';
+import MovieOverview from './MovieOverview';
+import { useParams } from 'react-router-dom';
+import BackButton from './BackButton';
+const MovieDetails = () => {
+    const {movie_id} = useParams();
+    return (
+        <div>
+            <div><BackButton /></div>
+            <div>
+                <MovieOverview type={'movie'} movieId={movie_id} credits={""}/>
+                <div className=''>
+                    
+                    <CastingDetails type={'movie'} movieId={movie_id} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieDetails
