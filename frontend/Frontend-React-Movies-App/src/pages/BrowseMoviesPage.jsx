9164109444763e36732eaa1c0acf56035cfbe328
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2025-10-06 11:09:55 +0530

    Dedicated page to browse Movies - Under Developnment

---
+++ frontend/Frontend-React-Movies-App/src/pages/BrowseMoviesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BrowseMovies from '../components/BrowseMovies'
+
+const BrowseMoviesPage = () => {
+  return (
+    <div className='min-h-screen'>
+        <BrowseMovies />
+    </div>
+  )
+}
+
+export default BrowseMoviesPage
