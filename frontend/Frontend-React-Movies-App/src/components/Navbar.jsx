eebf254511f09d5a5706d5e0e534596c4407c2e0
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2025-10-09 16:28:25 +0530

    Updated styles for Navbar

--- frontend/Frontend-React-Movies-App/src/components/Navbar.jsx
+++ frontend/Frontend-React-Movies-App/src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import HeaderItems from './HeaderItems';
 
+import { SiThemoviedatabase } from "react-icons/si";
+import { Link } from 'react-router-dom';
+import { RiMovieAiFill } from "react-icons/ri";
+
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
     return (
@@ -9,6 +13,12 @@ const Navbar = () => {
             <div className='md:hidden'>
                 <div className='flex justify-between items-center p-4 bg-blue-500 text-white'>
                     <div onClick={() => {setShowMenu(!showMenu)}} className=''><GiHamburgerMenu /></div>
+                    <Link to="/">
+                        <div className='flex w-full text-xl md:text-2xl items-center gap-2 text-white'>
+                            <div className='bg-white shadow-lg rounded-full p-2 text-black'><SiThemoviedatabase size={35}/></div>
+                            <div className='flex items-center gap-2 bg-white px-4 shadow-lg py-2 text-black rounded-full'>Cine <RiMovieAiFill /> Verse</div>
+                        </div>
+                    </Link>
                 </div>
                 {showMenu ? <HeaderItems showMenu={showMenu} setShowMenu={setShowMenu} /> : ''}
             </div>
