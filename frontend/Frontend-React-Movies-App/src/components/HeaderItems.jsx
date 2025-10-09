ec22f2bed815b06ce02cdec82df5e7d4cf304bcd
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2025-10-09 16:27:50 +0530

    Update: Styles for the NavBar

--- frontend/Frontend-React-Movies-App/src/components/HeaderItems.jsx
+++ frontend/Frontend-React-Movies-App/src/components/HeaderItems.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { SiThemoviedatabase } from "react-icons/si";
+import { RiMovieAiFill } from "react-icons/ri";
 
 const HeaderItems = ({ showMenu, setShowMenu }) => {
   const linkStyle =
@@ -9,11 +10,16 @@ const HeaderItems = ({ showMenu, setShowMenu }) => {
 
   return (
     <div
-      className={`md:flex md:flex-row flex-col gap-2 md:gap-4 shadow-2xl items-center justify-between bg-black md:bg-blue-400 p-4 ${
+      className={`md:flex md:flex-row flex-col gap-2 md:gap-4 shadow-2xl items-center justify-between bg-blue-300 md:bg-blue-400 p-4 ${
         showMenu ? 'flex' : 'hidden md:flex'
       }`}
     >
-        <div className='text-md md:text-2xl w-[40%] flex items-center gap-2 text-white'><SiThemoviedatabase />Multi-Verse</div>
+        <Link to="/">
+          <div className='hidden md:flex w-full text-xl md:text-2xl items-center gap-2 text-white'>
+            <div className='bg-white shadow-lg rounded-full p-2 text-black'><SiThemoviedatabase size={35}/></div>
+            <div className='flex items-center gap-2 bg-white px-4 shadow-lg py-2 text-black rounded-full'>Cine <RiMovieAiFill /> Verse</div>
+          </div>
+        </Link>
         <div className='flex flex-col md:flex-row w-[60%] items-center gap-2 justify-between'>
           <Link to="/"><div className={linkStyle}>Home</div></Link>
           <Link to="/browse_movies"><div className={linkStyle}>Browse Movies</div></Link>
