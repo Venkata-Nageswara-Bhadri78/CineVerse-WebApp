87a201ad541fba4ce2c1398d27eb923f52477ddb
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2025-10-09 14:32:52 +0530

    Updated Choice Selector code

--- frontend/Frontend-React-Movies-App/src/API/ChoiceSelector.jsx
+++ frontend/Frontend-React-Movies-App/src/API/ChoiceSelector.jsx
@@ -23,3 +23,37 @@ export default function ChoiceSelector({selectChoice, setSelectChoice, isTopRate
         </Box>
     );
 }
+
+
+
+// import * as React from 'react';
+// import Select, { selectClasses } from '@mui/joy/Select';
+// import Option from '@mui/joy/Option';
+// import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
+
+// export default function ChoiceSelector({selectChoice, setSelectChoice, isTopRated}) {
+//   return (
+//     <Select
+//         value={selectChoice}
+//         onChange={(event) => {
+//             setSelectChoice(event.target.value)
+//         }}
+//       placeholder="Select a Choice"
+//       indicator={<KeyboardArrowDown />}
+//       sx={{
+//         width: 240,
+//         [`& .${selectClasses.indicator}`]: {
+//           transition: '0.2s',
+//           [`&.${selectClasses.expanded}`]: {
+//             transform: 'rotate(-180deg)',
+//           },
+//         },
+//       }}
+//     >
+//         <Option value="" disabled>Select Type</Option>
+//         {!isTopRated && <Option value="person">PERSON</Option>}
+//         <Option value={"tv"}>TV SHOW</Option>
+//         <Option value={"movie"}>MOVIE</Option>
+//     </Select>
+//   );
+// }
